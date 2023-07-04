94fc354353a74ef4eb73b2a0e2650f681ebd70c3
Himanshu Soni <71898557+imhimanshusoni@users.noreply.github.com>
2023-07-04 21:37:48 +0530

    Create solution.cpp

---
+++ Leetcode Daily Coding Challenge/Single Number II/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> ump;
+        for(auto it:nums) {
+            ump[it]++;
+        }
+        for(auto it:ump) {
+            if(it.second==1) return it.first;
+        }
+        return -1;
+    }
+};
